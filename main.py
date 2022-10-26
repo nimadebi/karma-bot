a935009f796f5a5741430f869df3b5a37c1b66c4
nimadepi <nakib@live.nl>
2022-10-26 21:09:10 +0200

    moved modules to main and further development

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 import os
-import dotenv
-from modules.identity import Identity
+import github_handler
+from dotenv import load_dotenv
+from discord import ui, app_commands
 
 
 class Client(discord.Client):
@@ -19,17 +20,33 @@ class Client(discord.Client):
         print(f"Logged in as {self.user}.")
 
 
-dotenv.load_dotenv()
+load_dotenv()
 discord_client = Client()
-tree = discord.app_commands.CommandTree(discord_client)
+tree = app_commands.CommandTree(discord_client)
 
 
-def main():
-    # Modules
-    Identity(discord_client, tree)
-    # Payment(discord_client, tree)
-    discord_client.run(os.getenv("discord-key"))
+class TestForm(discord.ui.Modal, title="test form"):
+    account = discord.ui.TextInput(label="Test input", style=discord.TextStyle.short, placeholder="holdtest",
+                                  required=True)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Your answer was {self.account.value}.")
 
 
-if __name__ == '__main__':
-    main()
+@tree.command(guild=discord.Object(696335510037332020), name="account", description="add or update your account")
+async def modal(interaction: discord.Interaction):
+    if not github_handler.is_in_identities(interaction.user.id):
+        await interaction.response.send_message("You are not in the list of contributors.")
+        return
+    await interaction.response.send_modal(TestForm())
+
+
+@tree.command(guild=discord.Object(696335510037332020), name="whitelist", description="whitelist an account")
+async def whitelist(interaction: discord.Interaction, account: discord.User):
+    """This command is only available to certain people.
+    TODO: add support for type 1 accounts (groups)"""
+    # await interaction.response.send_message("You are not whitelisted to use this command.")
+    github_handler.add_identity("None", 0, account.id, str(account), None, None)
+    await interaction.response.send_message(f"We have added {str(account)} to the whitelist.")
+
+discord_client.run(os.getenv("discord-key"))
