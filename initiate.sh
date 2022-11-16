b3d7bc1a4b1955cc406ccd8ba22dcb543eaf86d8
Paulito <paulito123.work@gmail.com>
2022-11-15 22:35:15 -0600

    Fixed path issue

--- initiate.sh
+++ initiate.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # get current path
-CURRENT_PATH=$(pwd)
+this_file=$(realpath $0)
+PYTHONPATH=$(dirname $this_file)
 
 # create necessary directory for ol tool to function
 [ ! -d "~/.0L" ] && mkdir ~/.0L
 
 # copy the ol tool config file
-cp $CURRENT_PATH/assets/0L.toml ~/.0L
+cp $PYTHONPATH/assets/0L.toml ~/.0L
 
 # make the ol tool executable
-chmod +x $CURRENT_PATH/bin/ol
+chmod +x $PYTHONPATH/bin/ol
 
 # add the path of the ol tool to the PATH variable. 
 # This should be done every time you logout and login
 # again. To prevent this, you can add this line at
 # the bottom of the .bashrc file in your home dir.
-export PATH="$CURRENT_PATH/bin:$PATH"
+export PATH="$PYTHONPATH/bin:$PATH"
